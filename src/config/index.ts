c13e803652694c97eb2930054d6d7559918cb77f
tanimahmed07 <tanimislamt@gmail.com>
2026-03-16 22:55:54 +0600

    server initial setup done

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  port: process.env.PORT || 5000,
+  database_url: process.env.MONGODB_URI,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || 12,
+  jwt_secret: process.env.JWT_SECRET,
+  jwt_expires_in: process.env.JWT_EXPIRES_IN,
+  gemini_api_key: process.env.GEMINI_API_KEY,
+};
